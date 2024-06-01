7def41c3b8824cc09eef08febfc0313b56ef35ca
Ankit-47 <omkathet218@gmail.com>
2024-06-01 13:54:20 +0545

    ifelse statement example

---
+++ ifelse.py
@@ -0,0 +1,18 @@
+#syntax
+
+ #if condition:
+#do something
+
+#else
+#do something
+
+weather="rainy"
+if weather == "rainy":
+ print ("take umbrella")
+
+else :
+
+ print ("no need of umbrella")
+
+ #output will be take umbrella
+ 
