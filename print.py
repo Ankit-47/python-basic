d9a11d9acb6b217752041526708283b55b03f819
Ankit-47 <omkathet218@gmail.com>
2024-06-01 11:30:00 +0545

    hellow world

---
+++ print.py
@@ -0,0 +1,24 @@
+print("Hello world");
+
+#this is how you can comment singe line in python
+
+#operator-implementation
+
+a=10;
+b=20;
+c=30;
+sum=a+b+c;
+
+print("Sum=",sum);
+
+
+difference=b-a;
+print("Difference=",difference);
+
+print("multiplication=",a*b);
+print("float divisoin",b//a)
+
+
+#type casting
+
+
