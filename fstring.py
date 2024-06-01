8cfdf0e89a15955af773b45df1e3a1810676f58a
Ankit-47 <omkathet218@gmail.com>
2024-06-01 11:59:38 +0545

    concatination

---
+++ fstring.py
@@ -0,0 +1,12 @@
+
+
+
+#string cocatinate
+
+print("nosk"+"club") #output= noskclub
+
+print("nosk","club")#output = nosk club
+
+#print("nosk"+ 20) #output=eroor
+
+print("nosk",20) #string and int can be concatinate using , in between   {output= nosk 20}
